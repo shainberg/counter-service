2397045eb179d4d9cbf40eb0c88e8dadef0f8ba5
root <root@ip-10-0-1-19.eu-central-1.compute.internal>
2021-06-17 10:39:38 +0000

    eks node group

--- eks/eks/eks.tf
+++ eks/eks/eks.tf
@@ -6,6 +6,68 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.my-cluster.cluster_id
 }
 
+
+data "aws_vpc" "selected" {
+  filter {
+    name = "tag:Name"
+    values = ["ckp-vpc"]
+  }
+}
+
+data "aws_subnet" "ckp-subnet-public-1" {
+  vpc_id = data.aws_vpc.selected.id
+  filter {
+    name   = "tag:Name"
+    values = ["ckp-subnet-public-1"] # insert values here
+  }
+}
+
+data "aws_subnet" "ckp-subnet-public-2" {
+  vpc_id = data.aws_vpc.selected.id
+  filter {
+    name   = "tag:Name"
+    values = ["ckp-subnet-public-2"] # insert values here
+  }
+}
+
+data "aws_subnet" "ckp-subnet-public-3" {
+  vpc_id = data.aws_vpc.selected.id
+  filter {
+    name   = "tag:Name"
+    values = ["ckp-subnet-public-3"] # insert values here
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.example.name
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
@@ -16,15 +78,15 @@ module "my-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-cluster"
   cluster_version = "1.20"
-  subnets         = ["${aws_subnet.ckp-subnet-public-1.id}", "${aws_subnet.ckp-subnet-public-2.id}", "${aws_subnet.ckp-subnet-public-3.id}"]
-  vpc_id          = "${aws_vpc.ckp-vpc.id}"
+  subnets         = [data.aws_subnet.ckp-subnet-public-1.id, data.aws_subnet.ckp-subnet-public-2.id, data.aws_subnet.ckp-subnet-public-3.id]
+  vpc_id          = data.aws_vpc.selected.id
 }
 
 resource "aws_eks_node_group" "example" {
-  cluster_name    = data.aws_eks_cluster.cluster.cluster_name 
-  node_group_name = data.aws_eks_cluster.cluster.cluster_name
+  cluster_name    = data.aws_eks_cluster.cluster.id 
+  node_group_name = data.aws_eks_cluster.cluster.id
   node_role_arn   = aws_iam_role.example.arn
-  subnet_ids      = ["${aws_subnet.ckp-subnet-public-1.id}", "${aws_subnet.ckp-subnet-public-2.id}", "${aws_subnet.ckp-subnet-public-3.id}"]
+  subnet_ids      = [data.aws_subnet.ckp-subnet-public-1.id, data.aws_subnet.ckp-subnet-public-2.id, data.aws_subnet.ckp-subnet-public-3.id]
 
   scaling_config {
     desired_size = 1
@@ -32,8 +94,10 @@ resource "aws_eks_node_group" "example" {
     min_size     = 1
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  remote_access {
+    ec2_ssh_key = "ckp-region-key-pair"
+  }
+
   depends_on = [
     aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
