bf655d44dcddaee509084868f546b84676f91e71
Rivka Kremer <rivka.k@develeap.com>
2021-06-28 01:09:41 +0300

    Updated  image verison to 15

--- create-flux.sh
+++ create-flux.sh
@@ -1,10 +1,15 @@
 export GHUSER="RivkaKremer"
 export GHREPO="counter-service"
-kubectl create ns flux || true
+#kubectl create ns flux || true
 fluxctl install \
   --git-user=${GHUSER} \
   --git-branch=flask-eks-app \
   --git-email=${GHUSER}@users.noreply.github.com \
   --git-url=git@github.com:${GHUSER}/${GHREPO} \
-  --git-path=namespaces,workloads \
+  --git-path= \
   --namespace=flux | kubectl apply -f -
+
+# configure deploy key in github repository
+# fluxctl identity --k8s-fwd-ns flux
+sleep 30
+fluxctl sync --k8s-fwd-ns flux
