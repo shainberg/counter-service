ae9a7ca36074bd8101837a18c99040f0ad8dcd58
Rivka Kremer <rivka.k@develeap.com>
2021-06-27 16:51:58 +0300

    add jenkins

---
+++ jenkins/main.tf
@@ -0,0 +1,91 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
+variable "ingressrules" {
+  type    = list(number)
+  default = [80, 443, 22]
+}
+
+resource "aws_security_group" "web_traffic" {
+  name        = "Allow web traffic"
+  description = "Allow ssh and standard http/https ports inbound and everything outbound"
+  vpc_id = "vpc-049b0fd153a683412"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "jenkins" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  subnet_id       = "subnet-0f4113029dfbc0d1d"
+  security_groups = [aws_security_group.web_traffic.id]
+  key_name        = "check-point"
+  associate_public_ip_address = true
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update -qq",
+      "sudo apt install -y default-jre",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
+      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
+      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
+      "sudo apt-get -y install iptables-persistent",
+      "sudo ufw allow 8080",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("~/.ssh/check-point.pem")
+  }
+
+  tags = {
+    "Name"      = "Jenkins_Server"
+    "Terraform" = "true"
+  }
+}
