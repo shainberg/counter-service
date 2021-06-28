e507ad2263510dc2e17085e8c5ba112d9222ba00
Rivka Kremer <rivka.k@develeap.com>
2021-06-28 16:47:34 +0300

    add another 4 replicas

--- create-flux.sh
+++ create-flux.sh
@@ -1,13 +1,22 @@
 export GHUSER="RivkaKremer"
 export GHREPO="counter-service"
 #kubectl create ns flux || true
-fluxctl install \
-  --git-user=${GHUSER} \
-  --git-branch=flask-eks-app \
-  --git-email=${GHUSER}@users.noreply.github.com \
-  --git-url=git@github.com:${GHUSER}/${GHREPO} \
-  --git-path= \
-  --namespace=flux | kubectl apply -f -
+#fluxctl install \
+#  --git-user=${GHUSER} \
+#  --git-branch=flask-eks-app \
+#  --git-email=${GHUSER}@users.noreply.github.com \
+#  --git-url=git@github.com:${GHUSER}/${GHREPO} \
+#  --git-path= \
+#  --namespace=flux | kubectl apply -f -
+
+flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
+  --owner=RivkaKremer \
+  --repository=counter-service \
+  --branch=flask-eks-app \
+  --path=./ \
+  --token-auth \
+  --personal
 
 # configure deploy key in github repository
 # fluxctl identity --k8s-fwd-ns flux
