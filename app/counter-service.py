c43cf12cc08a35a006ac8493ef159745b365e330
Eilon01 <93954447+Eilon01@users.noreply.github.com>
2024-09-02 22:47:39 +0300

    Update counter-service.py
    updated app

--- app/counter-service.py
+++ app/counter-service.py
@@ -1,9 +1,14 @@
 #!flask/bin/python
+import os
 import redis
 from flask import Flask, request, request_started
 
+# Get env variables from configMap, if no values exist set to localhost:6379
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+redis_port = int(os.getenv('REDIS_PORT', 6379))
+
 # Connect to redis
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 # Define key name
 key = 'counter'
@@ -17,7 +22,7 @@ def index():
         
         # Increase counter by 1 and update variable
         r.incr(key)
-        counter = int(r.get(key).decode('utf-8'))
+        counter = int(r.get(key))
 
         return "Hmm, Plus 1 please "
     
