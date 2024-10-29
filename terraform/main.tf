073524536f29db9631dabc9ca4dec40552ebbf2c
rusver <www1ruslan@gmail.com>
2024-10-29 08:10:59 +0200

    added files terraform and Deployments

---
+++ terraform/main.tf
@@ -0,0 +1,428 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  region = var.region
+}
+
+
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
+}
+
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  cluster_name = "rus-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "rus-eks-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = { Service = "eks.amazonaws.com" },
+        Action    = "sts:AssumeRole",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_role_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"  # Grants EKS permissions
+}
+
+resource "aws_iam_policy" "eks_node_policy" {
+  name        = "EKSNodePolicy"
+  description = "Policy to allow EKS nodes to access required EC2 actions"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          # Add any additional required actions here
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+/*
+resource "aws_iam_role_policy_attachment" "attach_eks_node_policy" {
+  role       = "node-group-1-eks-node-group-<unique-id>" # Replace with your actual role name
+  policy_arn = aws_iam_policy.eks_node_policy.arn
+}
+*/
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.24.1"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.31"
+
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  cluster_upgrade_policy = {  # Add this block
+    support_type = "STANDARD"  # Set to STANDARD or EXTENDED as needed
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "Rus-AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+#------------------------
+#install aws LoadBalancer
+
+resource "aws_iam_policy" "alb_controller_policy" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "IAM policy for ALB Ingress Controller"
+  policy      = <<EOF
+  { "Version": "2012-10-17", "Statement": [{
+        "Effect": "Allow",
+        "Action": [
+            "acm:DescribeCertificate",
+            "acm:ListCertificates",
+            "acm:GetCertificate",
+            "ec2:AuthorizeSecurityGroupIngress",
+            "ec2:RevokeSecurityGroupIngress",
+            "ec2:CreateSecurityGroup",
+            "ec2:DeleteSecurityGroup",
+            "ec2:DescribeAccountAttributes",
+            "ec2:DescribeAddresses",
+            "ec2:DescribeAvailabilityZones",
+            "ec2:DescribeInternetGateways",
+            "ec2:DescribeVpcs",
+            "ec2:DescribeVpcPeeringConnections",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeSecurityGroups",
+            "ec2:DescribeInstances",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DescribeTags",
+            "ec2:GetCoipPoolUsage",
+            "ec2:DescribeCoipPools",
+            "elasticloadbalancing:DescribeLoadBalancers",
+            "elasticloadbalancing:DescribeLoadBalancerAttributes",
+            "elasticloadbalancing:DescribeListeners",
+            "elasticloadbalancing:DescribeListenerCertificates",
+            "elasticloadbalancing:DescribeSSLPolicies",
+            "elasticloadbalancing:DescribeRules",
+            "elasticloadbalancing:DescribeTargetGroups",
+            "elasticloadbalancing:DescribeTargetGroupAttributes",
+            "elasticloadbalancing:DescribeTargetHealth",
+            "elasticloadbalancing:DescribeTags",
+            "elasticloadbalancing:CreateListener",
+            "elasticloadbalancing:DeleteListener",
+            "elasticloadbalancing:CreateRule",
+            "elasticloadbalancing:DeleteRule",
+            "elasticloadbalancing:SetRulePriorities",
+            "elasticloadbalancing:ModifyRule",
+            "elasticloadbalancing:AddTags",
+            "elasticloadbalancing:RemoveTags",
+            "elasticloadbalancing:ModifyListener",
+            "elasticloadbalancing:ModifyLoadBalancerAttributes",
+            "elasticloadbalancing:SetIpAddressType",
+            "elasticloadbalancing:SetSecurityGroups",
+            "elasticloadbalancing:SetSubnets",
+            "elasticloadbalancing:DeleteLoadBalancer",
+            "elasticloadbalancing:CreateTargetGroup",
+            "elasticloadbalancing:ModifyTargetGroup",
+            "elasticloadbalancing:ModifyTargetGroupAttributes",
+            "elasticloadbalancing:DeleteTargetGroup",
+            "elasticloadbalancing:RegisterTargets",
+            "elasticloadbalancing:DeregisterTargets",
+            "iam:CreateServiceLinkedRole",
+            "iam:GetServerCertificate",
+            "iam:ListServerCertificates",
+            "cognito-idp:DescribeUserPoolClient",
+            "waf-regional:GetWebACL",
+            "waf-regional:GetWebACLForResource",
+            "waf-regional:AssociateWebACL",
+            "waf-regional:DisassociateWebACL",
+            "wafv2:GetWebACL",
+            "wafv2:GetWebACLForResource",
+            "wafv2:AssociateWebACL",
+            "wafv2:DisassociateWebACL",
+            "shield:GetSubscriptionState",
+            "shield:DescribeProtection",
+            "shield:CreateProtection",
+            "shield:DeleteProtection"
+        ],
+        "Resource": "*"
+    }
+  ]
+  }
+  EOF
+}
+
+# EKS Cluster Data Sources
+data "aws_eks_cluster" "my_cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "my_cluster_auth" {
+  name = module.eks.cluster_name
+}
+
+# Create IAM Role for the Controller
+resource "aws_iam_role" "alb_controller_role" {
+  name               = "EKS_ALB_Controller"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": { "Service": "eks.amazonaws.com" },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Associate IAM Role with the EKS Cluster
+resource "aws_iam_role_policy_attachment" "alb_controller_policy_attach" {
+  role       = aws_iam_role.alb_controller_role.name
+  policy_arn = aws_iam_policy.alb_controller_policy.arn
+}
+
+# Install the AWS Load Balancer Controller via Helm
+resource "kubernetes_config_map" "aws_auth" {
+  depends_on = [module.eks, aws_iam_role_policy_attachment.alb_controller_policy_attach]
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = <<EOF
+    - rolearn: ${aws_iam_role.alb_controller_role.arn}
+      username: system:node:{{SessionName}}
+      groups:
+        - system:masters
+    EOF
+  }
+}
+
+# Install the AWS Load Balancer Controller via Helm
+
+provider "kubernetes" {
+  config_path = "C:/Users/Ruslan/.kube/config"
+  host                   = data.aws_eks_cluster.my_cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.my_cluster_auth.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my_cluster.certificate_authority[0].data)
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "C:/Users/Ruslan/.kube/config"
+    host                   = data.aws_eks_cluster.my_cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
+
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  chart      = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  namespace  = "kube-system"
+
+  timeout    = 600  # Increase timeout to 600 seconds
+
+  set {
+    name  = "clusterName"
+    value = "module.eks.cluster_name"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "region"
+    value = "eu-west-1"
+  }
+}
+
+
+
+
+
+
+
+#==================================
+/*
+#installing ingress for that cluster
+
+
+
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.my_cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "helm" {
+  kubernetes {
+    
+    host                   = data.aws_eks_cluster.my_cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://charts.helm.sh/stable"
+  chart      = "nginx-ingress"
+  version    = "3.7.0" #Specify a suitable version
+  
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer" # This will create a LoadBalancer for external access
+  }
+}
+
+resource "kubernetes_ingress" "my_ingress" {
+  metadata {
+    name = "my-ingress"
+    annotations = {
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+
+  spec {
+    rule {
+      host = "eu-west-1.elb.amazonaws.com" # Change to your domain or leave as is for testing
+
+      http {
+        path {
+          path = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "your-service-name" # Change to your service name
+              port {
+                number = 80 # Change to your service port
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+*/
